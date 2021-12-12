a4831460c21ec8adbe9ee4664221e6733a2ad755
Leonardo Manrich <leomanrich@hotmail.com>
2021-12-12 20:43:48 -0300

    Update index.php

--- src/index.php
+++ src/index.php
@@ -11,23 +11,12 @@ use Leonardomanrich\Cpanelwhm\Modules\Databases\list_databases;
 echo "<pre>";
 
 $environment = new UAPI(
-        'https://allrise.com.br',
+        'h',
         Ports::SECURE_API,
-        'allris02', 
-        'qF81q16Lfa'
+        '', 
+        ''
     );
 
 $cpanelClient = new HttpClient($environment);
 
 var_dump($cpanelClient->execute(new list_databases())); */
-
-$client = new GuzzleHttp\Client([
-    'base_uri' => 'https://allrise.com.br:2083/execute/',
-    'headers' => [
-        'Authorization' => 'Basic YWxscmlzMDI6cUY4MXExNkxmYQ=='
-    ]
-]);
-
-$response = $client->request('GET', 'Mysql/list_databases');
-
-var_dump(json_decode($response->getBody()->getContents()));
